4c1b56f537a75c9381fe2bef3302c0a290d971b4
Jason Kottler <jason.kottler@skyscanner.net>
2017-07-10 16:35:35 +0100

    Move code from html to js
    Setup "concurrently" to watch both frontend and backend changes.
    
    Split project into app and server folders.

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,8 @@ const game = require('./game');
 
 const app = express();
 
+app.use('/static', express.static('bin'))
+
 app.get('/', function (req, res) {
   res.sendFile('./index.html', { root: __dirname });
 })
@@ -11,6 +13,8 @@ app.get('/new', (req, res) => {
   let result = game.newGame()
   res.send(result);
 })
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
