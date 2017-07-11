bdd74d5ed400c501bb4009e6ee24a9f87eb20933
Jason Kottler <jason.kottler@skyscanner.net>
2017-07-11 19:42:26 +0100

    Assign uuid to each new game
    Also read wordlist from server folder

--- src/server/game.js
+++ src/server/game.js
@@ -1,9 +1,10 @@
 const _ = require('lodash');
 const fs = require('fs');
+const uuid = require('uuid/v4');
 
 const card = require('./card');
 
-const words = fs.readFileSync('codenames-word-list.txt').toString().split('\n');
+const words = fs.readFileSync(`${__dirname}/codenames-word-list.txt`).toString().split('\n');
 
 class Game {
   constructor() {
@@ -11,6 +12,7 @@ class Game {
     this.cards = [];
     this.side = undefined; // side is 'red' or 'blue'
     this.phase = 'clue' // phase is 'clue' or 'guess'
+    this.id = uuid();
     this.initCards();
   }
 
@@ -51,47 +53,6 @@ class Game {
     });
   }
 }
-
-// var newGame = () => {
-//   var result = {};
-//   var wordList = _.sampleSize(words, 25); // get words for game
-
-//   var teams = []; // an array to hold all the teams to be assigned for this game
-//   var first = _.sample(['red', 'blue']); // set first player
-//   result.firstPlayer = first;
-//   teams.push(first);
-
-//   // build layout of teams
-//   const teamSize = {
-//     red: 8,
-//     blue: 8,
-//     bystander: 7,
-//     assassin: 1,
-//   };
-
-//   for (var team in teamSize) {
-//     // if (teamSize.hasOwnProperty(team)) {
-//     for (let i = 0; i < teamSize[team]; i++) {
-//       teams.push(team);
-//       // }
-//     }
-//   }
-
-//   result.cards = [];
-//   wordList.forEach(function (word) {
-//     // deal random team
-//     var index = _.random(0, teams.length - 1);
-//     var team = teams[index];
-//     teams.splice(index, 1);
-
-//     // make new card
-//     var newCard = new card.card(word, false, team);
-
-//     result.cards.push(newCard);
-//   });
-
-//   return result;
-// };
 
 module.exports = {
   Game,
